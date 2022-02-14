cd022027680895375d4873c2160adf7d02b61cfb
jgabari <j.gabari.2017@alumnos.urjc.es>
2022-02-14 11:57:35 +0100

    ej 2 variables

---
+++ P1/S2/ej-02-variables.js
@@ -0,0 +1,13 @@
+// Probando variables y como imprimirlas en la consola
+
+// Numerica
+let n = 3;
+
+// Imprimir directamente
+console.log("variable n: ", n);
+
+// Dentro de la cadena
+console.log(`variable n: ${n}`);
+
+// Concatenar al mensaje
+console.log("variable n: " + n);
