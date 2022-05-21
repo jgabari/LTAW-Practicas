7dd031af3e502db8f2822dff6d7e2df9b296232e
jgabari <javigabari@gmail.com>
2022-05-21 11:40:21 +0200

    Version 3

--- P4/Ejemplos/main.js
+++ P4/Ejemplos/main.js
@@ -3,7 +3,16 @@ const electron = require('electron');
 
 console.log("Arrancando electron...");
 
+// Variable para acceder a la ventana
+let win = null;
+
 // Punto de entrada
 electron.app.on('ready', () => {
     console.log("Evento Ready!");
+
+    // Crear la ventana principal
+    win = new electron.BrowserWindow({
+        width: 600,
+        height: 400
+    });
 })
